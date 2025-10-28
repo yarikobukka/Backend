170c991304cafd2a3355a700b579f152144bfef1
Akito <akikoshi0718@gmail.com>
2025-10-28 15:51:39 +0900

    ちょっといじりました　byたまこし

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://web-one-beta-11.vercel.app"],
+    allow_origins=[
+        "https://web-one-beta-11.vercel.app",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
