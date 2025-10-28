b72711858acd790f4add8ac521b8da74722d9d2b
Akito <akikoshi0718@gmail.com>
2025-10-28 17:50:16 +0900

    はてはて

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ app = FastAPI()
 # CORS設定：フロントエンドからのアクセスを許可
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://web-one-beta-11.vercel.app"],
+    allow_origins=[
+        "https://web-one-beta-11.vercel.app",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 # リクエストボディの定義
 class BookRequest(BaseModel):
     title: str
